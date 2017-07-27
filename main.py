fc90e5ff0cc720976b8dc010780e7a97fcfd9b52
Brad Ofrim <bofrim@gmail.com>
2017-07-26 22:55:44 -0600

    Add symbols list

--- main.py
+++ main.py
@@ -1,39 +1,9 @@
 # The Young Stock Observer
 # Brad Ofrim & Abid Rahman
-'''
-Command line interface for simulating an investment account
-Give user ability to pull data related to a specitic stock
-Display data for the user
-Allow user to login to an account or create a new account
-Option to delete existing accounts
-New accounts should be created with initial funds
-'''
 
-'''
-Organization:
-    Classes:
-        - Application:
-            * Starting point
-            * Initializes resources
-            * Connects to web services
-            * Directs to 'login' options
-
-        - Account:
-            * Hold account info
-            * Balances
-            * Holdings
-            * Quantities
-            * Purchase prices
-            * Changes
-
-        - Stock
-            * Data pertaining to the stock
-'''
 import sys
 
 import csv
-import stocks
-from helper import *
 from rtstock.stock import Stock
 
 def main():
@@ -42,6 +12,8 @@ def main():
 		reader = csv.reader(symbol_file)
 		for symbol in reader:
 			latest_stock_price = Stock(symbol).get_latest_price()[0]
+			print(symbol)
+			print(latest_stock_price[u'LastTradePriceOnly'])
 			if latest_stock_price[u'LastTradePriceOnly'] < 2.00:
 				short_list.append(symbol)
 
